03cc6b725be24919c865c8e8a78fd13b7a507efe
Frederik Wilde <wilde.physics@gmail.com>
2022-04-27 17:18:41 +0200

    Added implementation with JAX

--- all_bitstrings.py
+++ all_bitstrings.py
@@ -1,5 +1,10 @@
 import numpy as np
+import jax.numpy as jnp
+import jax
 import itertools
+from functools import partial
+
+jax.config.update("jax_enable_x64", True)
 
 def all_bitstrings(size):
     bitstrings = np.ndarray((2**size, size), dtype=int)
@@ -16,3 +21,21 @@ def all_bitstrings_slow(size):
 
 def all_bitstrings_iterator(size):
     return itertools.product([0, 1], repeat=size)
+
+@partial(jax.jit, static_argnums=(1,))
+def cycle_step(counter_period, _):
+    counter, period = counter_period
+    counter = jax.lax.cond(counter >= period-1, lambda: -period, lambda: counter+1)
+    bit = jax.lax.cond(
+        counter >= 0,
+        lambda: jnp.array(0, dtype=jnp.int64),
+        lambda: jnp.array(1, dtype=jnp.int64)
+    )
+    return (counter, period), bit
+
+def build_column(index, size):
+    counter_period = (-1, 2 ** index)
+    return jax.lax.scan(cycle_step, counter_period, None, 2**size)[1]
+
+def all_bitstrings_jax(size):
+    return jax.vmap(lambda i: build_column(i, size), out_axes=1)(jnp.arange(size-1, -1, -1))
